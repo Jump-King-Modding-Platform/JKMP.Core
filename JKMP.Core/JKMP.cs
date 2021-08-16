6a355615b54e0a7b9380d65fdbfa9b3dbe3eb1fb
Skipcast <jonathanjl99@gmail.com>
2021-08-16 16:36:12 +0200

    Updated gitignore and added project files

---
+++ JKMP.Core/JKMP.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace JKMP.Core
+{
+    public static class JKMP
+    {
+        public static void Initialize()
+        {
+            Console.WriteLine("Initializing JKMP!");
+        }
+    }
+}
