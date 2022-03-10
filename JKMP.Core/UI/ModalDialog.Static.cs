d7544f6bb4cd50b6de001e88259fcdb230bbcc06
Skipcast <jonathanjl99@gmail.com>
2022-03-10 01:28:35 +0100

    Added more static dialog methods to make it simpler to show common modal types

--- JKMP.Core/UI/ModalDialog.Static.cs
+++ JKMP.Core/UI/ModalDialog.Static.cs
@@ -8,7 +8,6 @@ namespace JKMP.Core.UI
 
         /// <summary>
         /// Displays a modal dialog. onClick is called when the user clicks any of the buttons.
-        /// The window is automatically closed afterwards.
         /// </summary>
         /// <param name="message">The message to display.</param>
         /// <param name="onClick">
@@ -18,15 +17,59 @@ namespace JKMP.Core.UI
         /// <param name="buttonNames">The text for each button.</param>
         public static void ShowDialog(string message, Action<int?>? onClick = null, params string[] buttonNames)
         {
+            ThrowIfNotInitialized();
+            
+            var dialog = new ModalDialog(message, buttonNames, onClick);
+            instance!.PushModal(dialog);
+        }
+
+        /// <summary>
+        /// Displays a modal info dialog. The only option is "OK". The callback is invoked when the modal is closed.
+        /// </summary>
+        /// <param name="message">The message to display</param>
+        /// <param name="onClosed">The callback to invoke when the modal is closed. Can be null.</param>
+        public static void ShowInfo(string message, Action? onClosed = null)
+        {
+            ThrowIfNotInitialized();
+
+            Action<int?>? callback = null;
+
+            if (onClosed != null)
+            {
+                callback = _ => onClosed();
+            }
+
+            var dialog = new ModalDialog(message, new[] { "OK" }, callback);
+            instance!.PushModal(dialog);
+        }
+
+        /// <summary>
+        /// Displays a modal with yes and no options. The callback is invoked when the modal is closed.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
+        /// <param name="onClosed">The callback to invoke when the modal is closed.</param>
+        /// <exception cref="ArgumentNullException">Thrown is onClosed is null.</exception>
+        public static void ShowConfirm(string message, Action<bool> onClosed)
+        {
+            if (onClosed == null) throw new ArgumentNullException(nameof(onClosed));
+            
+            ThrowIfNotInitialized();
+
+            var dialog = new ModalDialog(message,
+                new[] { "Yes", "No" },
+                index => { onClosed(index == 0); }
+            );
+
+            instance!.PushModal(dialog);
+        }
+
+        private static void ThrowIfNotInitialized()
+        {
             if (instance == null)
             {
                 // Should never happen since the modal manager is initialized before the menu is fully created.
                 throw new InvalidOperationException("ModalManager is not initialized.");
             }
-
-            var dialog = new ModalDialog(message, buttonNames, onClick);
-
-            instance.PushModal(dialog);
         }
     }
 }
