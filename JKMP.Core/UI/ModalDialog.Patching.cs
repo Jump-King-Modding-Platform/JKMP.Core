07cdc0a2fb4c89df462c3beab6d1fd031fb2a8e0
Skipcast <jonathanjl99@gmail.com>
2022-03-12 17:47:07 +0100

    Remove unused BT node type

--- JKMP.Core/UI/ModalDialog.Patching.cs
+++ JKMP.Core/UI/ModalDialog.Patching.cs
@@ -14,34 +14,5 @@ namespace JKMP.Core.UI
             var result = new BTsimultaneous(instance, mainMenu);
             return result;
         }
-
-        /// <summary>
-        /// A node that runs the children in the order they're added. Only proceeds to run the next child if the previous one returns Success or Failure.
-        /// Returns Success if all children return Success. Returns Failure if no child returns Running.
-        /// </summary>
-        private class BTPriorityComposite : IBTcomposite
-        {
-            public BTPriorityComposite(params IBTnode[] children) : base(children)
-            {
-            }
-            
-            protected override BTresult MyRun(TickData tickData)
-            {
-                bool allSuccess = true;
-                
-                foreach (IBTnode node in Children)
-                {
-                    BTresult result = node.Run(tickData);
-
-                    if (result is BTresult.Running)
-                        return BTresult.Running;
-
-                    if (result != BTresult.Success)
-                        allSuccess = false;
-                }
-
-                return allSuccess ? BTresult.Success : BTresult.Failure;
-            }
-        }
     }
 }
